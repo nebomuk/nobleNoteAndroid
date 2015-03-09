d0e8b19fdfaad9e17554ad25694e3e7aa3073fc4
Taiko <taiko000@gmail.com>
2015-03-09 23:30:36 +0100

    added sharing button, fixed contextual action bar

--- app/src/main/java/com/taiko/noblenote/SelectionActionModeCallback.java
+++ app/src/main/java/com/taiko/noblenote/SelectionActionModeCallback.java
@@ -2,14 +2,17 @@ package com.taiko.noblenote;
 
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
+import android.text.SpannableString;
 import android.text.style.StrikethroughSpan;
 import android.text.style.UnderlineSpan;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 
 /**
@@ -21,22 +24,25 @@ import android.view.MenuItem;
  */
 
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class SelectionActionModeCallback implements android.view.ActionMode.Callback {
 
 	
 	private DroidWriterEditText editText;
-	
+    private ShareActionProvider mShareActionProvider;
+
     public SelectionActionModeCallback(DroidWriterEditText editText) {
 		super();
 		this.editText = editText;
 	}
-    
+
     @Override
 	public boolean onCreateActionMode(ActionMode mode, Menu menu) {
         MenuInflater inflater = mode.getMenuInflater();
         inflater.inflate(R.menu.selection_cab, menu);
-        menu.removeItem(android.R.id.selectAll);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
         return true;
     }
 	@Override
@@ -50,32 +56,46 @@ public class SelectionActionModeCallback implements android.view.ActionMode.Call
 
         switch(item.getItemId()) {
 
-        case R.id.bold: 
-        {
-        	Format.toggleStyleSpan(this.editText,Typeface.BOLD);
-        	editText.setModified(true); // apparently doesnt get set automatically by DroidWriterEditText's TextWatcher
-            return true;
+            case R.id.bold: {
+                Format.toggleStyleSpan(this.editText, Typeface.BOLD);
+                editText.setModified(true); // apparently doesnt get set automatically by DroidWriterEditText's TextWatcher
+                return true;
+            }
+            case R.id.italic: {
+                Format.toggleStyleSpan(this.editText, Typeface.ITALIC);
+                editText.setModified(true);
+                return true;
+            }
+            case R.id.underline: {
+                Format.toggleCharacterStyle(this.editText, UnderlineSpan.class);
+                editText.setModified(true);
+                return true;
+            }
+            case R.id.strikethrough: {
+                Format.toggleCharacterStyle(this.editText, StrikethroughSpan.class);
+                editText.setModified(true);
+                return true;
+            }
+            case R.id.menu_item_share: {
+                int selectionStart = editText.getSelectionStart();
+                int selectionEnd = editText.getSelectionEnd();
+                if (selectionStart > selectionEnd) {
+                    int tmp = selectionEnd;
+                    selectionEnd = selectionStart;
+                    selectionStart = tmp;
+                }
+                CharSequence selectedText = editText.getText().subSequence(selectionStart, selectionEnd);
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, selectedText.toString() /*deep copy*/); // sharing spanned string does not work here
+                sendIntent.setType("text/plain");
+                if (mShareActionProvider != null)
+                    mShareActionProvider.setShareIntent(sendIntent);
+                return true;
+            }
+
         }
-		case R.id.italic:
-        {
-        	Format.toggleStyleSpan(this.editText,Typeface.ITALIC);
-        	editText.setModified(true);
-            return true;
-        }
-        case R.id.underline:
-        {
-        	Format.toggleCharacterStyle(this.editText,UnderlineSpan.class);
-        	editText.setModified(true);
-            return true;
-        }
-        case R.id.strikethrough:
-        {
-        	Format.toggleCharacterStyle(this.editText,StrikethroughSpan.class);
-        	editText.setModified(true);
-        	return true;
-        }
-        
-        }  
         return false;
     }
 
@@ -84,6 +104,5 @@ public class SelectionActionModeCallback implements android.view.ActionMode.Call
     {
         editText.setWindowFocusWait(false);
 	}
-	
 
 }
